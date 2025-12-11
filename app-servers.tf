4f55b1fe7b7ee7765f442f2c03970ce9e81204dc
Raghu K <raghuk.vit@gmail.com>
2025-12-11 07:45:52 +0530

    Roboshop project

--- app-servers.tf
+++ app-servers.tf
@@ -5,7 +5,7 @@ resource "aws_launch_template" "app_instances" {
   image_id               = var.ami
   instance_type          = each.value["instance_type"]
   vpc_security_group_ids = [aws_security_group.app_sg[each.key].id]
-  user_data              = templatefile("${path.module}/userdata.sh", { component = each.key, env = var.env })
+  user_data              = base64encode(templatefile("${path.module}/userdata.sh", { component = each.key, env = var.env }))
 
   tag_specifications {
     resource_type = "instance"
