86536d05ca2f626f2d1044b61b37809b80a80f6e
Raghu K <raghuk.vit@gmail.com>
2025-12-12 07:51:39 +0530

    Roboshop project

--- userdata.sh
+++ userdata.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-dnf install python3.13-pip -y
-pip3.11 install ansible
+#dnf install python3.13-pip -y # Commented because these installation was done in golden image
+#pip3.11 install ansible # Commented because these installation was done in golden image
 ansible-pull -i localhost, -U https://github.com/raghudevopsb87/roboshop-ansible-templates.git main.yml -e component=${component} -e env=${env} | tee /root/user-data.log
